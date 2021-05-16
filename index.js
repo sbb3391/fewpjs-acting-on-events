ac1762b1476b567ff84e7652d2eca4b768db59e4
sbb3391 <sbb33391@gmail.com>
2021-05-16 05:06:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+// Your code here 
+dodger.style.right = "180px"
+
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+  let left = parseInt(leftNumbers, 10);
+
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`;
+    dodger.style.left = `${left + 1}px`;
+  }
+} 
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft() {
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+    dodger.style.right = `${right + 1}px`;
+  }
+} 
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+}); 
